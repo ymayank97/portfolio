5dfc66eccb6ec930a8f2be2789abd516513708a1
mayank <ymayank97@gmail.com>
2024-12-24 02:49:44 -0600

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ import dns from 'dns';
 dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  base: "/site",
+  }
 });
