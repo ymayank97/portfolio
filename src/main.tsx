aab9ca8ec1857ebaa960fcf26367820256ed4467
mayank <ymayank97@gmail.com>
2024-12-23 18:34:52 -0600

    minor fix dist folder

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
