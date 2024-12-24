5c86a3a14384c3b1bc6818c2e759881a89396a42
mayank <ymayank97@gmail.com>
2024-12-24 02:57:03 -0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/site/",
+  base: "/portfolio",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
